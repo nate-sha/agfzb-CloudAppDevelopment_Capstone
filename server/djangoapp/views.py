92837537de06f92686b7fc8dcb674ef8ca335c80
NaserShaar <shaar.nate@gmail.com>
2022-12-24 16:52:40 -0500

    Add authentication views and templates

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -29,20 +29,63 @@ def contact(request):
     return render(request, 'djangoapp/contact.jinja-html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+
+
+def login_request(request):
+    if request.method == "POST":
+        # Get the username and password provided by the user
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Authenticate the user
+        user = authenticate(username=username, password=password)
+        # if the user is authenticated
+        if user is not None:
+            # Longin the user
+            login(request, user)
+            # Display a success message
+            messages.success(request, "You have successfully logged in")
+            # Redirect to the index page (homepage)
+            return redirect('djangoapp:index')
+
+        else:
+            # User is not authenticated
+            messages.error(request, "Invalid username or password.")
+    return render(request, 'djangoapp/indexhtml')
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("djangoapp:index")
+
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        # Check if the username is already taken
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect('djangoapp:registration')
+        else:
+            user = User.objects.create_user(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                password=password, email=email)
+            user.save()
+            login(request, user)
+            messages.info(request, "You have successfully registered")
+            return redirect('djangoapp:index')
+    return render(request, 'djangoapp/registration.jinja-html')
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-
-
 def get_dealerships(request):
     context = {
         'title': 'Home'
